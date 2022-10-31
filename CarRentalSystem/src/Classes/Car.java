264975dfd829d17f43d5cabba17103100e7d022c
phinshuen <110078391+phinshuen@users.noreply.github.com>
2022-10-31 20:35:08 +0800

    Add files via upload

---
+++ CarRentalSystem/src/Classes/Car.java
@@ -0,0 +1,40 @@
+public class Car{
+    
+}
+
+/*
+ * Function name: addDates
+ * 
+ * What it does:
+ *  1. Add dates after the return car is approved
+ */
+
+/*
+ * Function name: removeDates
+ * 
+ * What it does:
+ *  1. Remove dates when payment for booking car is done
+ */
+
+/*
+ * Function name: carDetails / carCatalog
+ * 
+ * What it does:
+ *  1. Show the information of car
+ *  2. Information grouped by the types of car
+ *  3. After choosing types of car, customer should see the picture of the car? (discuss with bryan),
+ *  car model, seats, price
+ * 
+ */
+
+/*
+ * Discuss with Bryan
+ * Regarding creating classes for each car types
+ * 
+ * Format of car information: (discuss with Bryan)
+ *  1. Car types (Sedan, hatchback, Minivans, SUVs)
+ *  2. Car Name / Model
+ *  3. Seats (how many seater)
+ *  4. Price (subject to how many days)
+ *  5. Status (available or not available)
+ */
