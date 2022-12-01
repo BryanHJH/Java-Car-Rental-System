15ebd7620d481b35c514be3c339647e54f8132e8
Bryan Hor Jin Hao <2702bryan@gmail.com>
2022-12-01 22:04:37 +0800

    No major changes typo from previous commit

--- CarRentalSystem/src/Controllers/AdminMainPageController.java
+++ CarRentalSystem/src/Controllers/AdminMainPageController.java
@@ -7,7 +7,6 @@ import java.net.URL;
 import java.text.ParseException;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
